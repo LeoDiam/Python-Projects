14a4ff83b80dc70a279dc6079a2581939af2ec44
LeoDiam <t8190243@aueb.gr>
2021-03-13 11:00:51 +0200

    A simple random number project

---
+++ GuessTheNumber.py
@@ -0,0 +1,12 @@
+import random
+
+
+def guess(x):
+    random_num = random.randint(1, 20)
+    if random_num is x:
+        print("Great")
+    else:
+        print("Maybe next time.The number was " + random_num.__str__())
+
+
+guess(int(input()))
