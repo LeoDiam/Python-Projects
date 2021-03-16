e61b9f9acd44b15673bb0a94c0233b18a7fa23f2
LeoDiam <t8190243@aueb.gr>
2021-03-16 16:39:44 +0200

    approach_square for both negative and positive numbers

--- com/leo/python_easy_exercises/approach_square.py
+++ com/leo/python_easy_exercises/approach_square.py
@@ -1,16 +1,34 @@
 x = 25
-e = 0.01
+
 guesses = 0
-low = 0.0
-high = max(1.0 , x)
-ans = (high + low) / 2.0
-while abs(ans**2 - x) >= e:
-    print('low = ',low,' high = ',high,' ans ',ans)
-    guesses += 1
-    if ans**2 < x:
-        low = ans
-    else:
-        high = ans
+
+
+if x < 0:
+    e = 0.01
+    low = 0.0
+    high = min(-1.0, x)
     ans = (high + low) / 2.0
-print('num of guesses ',guesses)
-print(ans,'is close to square root of ',x)
+    while abs(ans ** 2 + x) >= e:
+        print('low = ', low, ' high = ', high, ' ans ', ans)
+        guesses += 1
+        if ans ** 2 < -x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low) / 2.0
+        print('num of guesses ', guesses)
+else:
+    e = 0.01
+    low = 0.0
+    high = max(1.0 , x)
+    ans = (high + low) / 2.0
+    while abs(ans ** 2 - x) >= e:
+        print('low = ', low, ' high = ', high, ' ans ', ans)
+        guesses += 1
+        if ans ** 2 < x:
+           low = ans
+        else:
+          high = ans
+        ans = (high + low) / 2.0
+        print('num of guesses ', guesses)
+print(ans, 'is close to square root of ', x)
