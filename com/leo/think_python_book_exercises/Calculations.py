c8d0e9dd0375b5446cbd06e97f8d637decde5426
LeoDiam <t8190243@aueb.gr>
2021-03-28 23:06:04 +0300

    calculations

--- com/leo/think_python_book_exercises/Calculations.py
+++ com/leo/think_python_book_exercises/Calculations.py
@@ -1,12 +1,18 @@
-def calculate_seconds(minutes,sec):
+def calculate_seconds(minutes, sec):
     return (minutes * 100) + sec
+
+
 def calculate_miles(miles):
     return miles * 1.61
-def average_race_pace(miles,minutes,sec):
-    return calculate_miles(miles) / ((calculate_seconds(minutes,sec)) / 100)
+
+
+def average_race_pace(miles, minutes, sec):
+    return calculate_miles(miles) / ((calculate_seconds(minutes, sec)) / 100)
+
+
 # def average_race_pace_display_m_and_s(miles,hours,sec):
 #        return miles % ((hours/60)*100 + sec)
 
-print(calculate_seconds(42,42))
+print(calculate_seconds(42, 42))
 print(calculate_miles(10))
-print(average_race_pace(10,42,42))
+print(average_race_pace(10, 42, 42))
